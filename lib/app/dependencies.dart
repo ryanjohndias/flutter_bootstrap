1105d75c8c0603ddb9e85cfa72a5e06c39c8b3a4
Ryan Dias <ryanjohndias@gmail.com>
2022-08-10 19:39:16 +0200

    prefs

--- lib/app/dependencies.dart
+++ lib/app/dependencies.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import '../data/api/swapi_api.dart';
+import '../data/persistence/key_value_storage.dart';
+import '../data/persistence/key_value_storage_impl.dart';
 import '../data/persistence/secure_storage.dart';
 import '../data/persistence/secure_storage_impl.dart';
 import '../data/repositories/example_repository.dart';
@@ -21,15 +23,21 @@ class Dependencies extends StatelessWidget {
         Provider<Dio>(
           create: (BuildContext context) => Dio(),
         ),
+        // Apis / Clients
         Provider<SwapiApi>(
           create: (BuildContext context) => SwapiApi(
             context.read(),
             baseUrl: AppConstants.baseUrl,
           ),
         ),
+        // Other
         Provider<SecureStorage>(
           create: (BuildContext context) => SecureStorageImpl(),
         ),
+        Provider<KeyValueStorage>(
+          create: (BuildContext context) => KeyValueStorageImpl(),
+        ),
+        // Repositories
         Provider<ExampleRepository>(
           create: (BuildContext context) => ExampleRepository(
             swapiApi: context.read(),
