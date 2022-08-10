91ef176cc0c7beb1bde944bcd1f9ba5546b4368c
Ryan Dias <ryanjohndias@gmail.com>
2022-08-10 19:24:04 +0200

    Secure Storage

--- lib/app/dependencies.dart
+++ lib/app/dependencies.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import '../data/api/swapi_api.dart';
+import '../data/persistence/secure_storage.dart';
+import '../data/persistence/secure_storage_impl.dart';
 import '../data/repositories/example_repository.dart';
 import 'app_constants.dart';
 
@@ -16,15 +18,23 @@ class Dependencies extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: <SingleChildWidget>[
-        Provider<Dio>(create: (BuildContext context) {
-          return Dio();
-        }),
-        Provider<SwapiApi>(create: (BuildContext context) {
-          return SwapiApi(context.read(), baseUrl: AppConstants.baseUrl);
-        }),
-        Provider<ExampleRepository>(create: (BuildContext context) {
-          return ExampleRepository(swapiApi: context.read());
-        }),
+        Provider<Dio>(
+          create: (BuildContext context) => Dio(),
+        ),
+        Provider<SwapiApi>(
+          create: (BuildContext context) => SwapiApi(
+            context.read(),
+            baseUrl: AppConstants.baseUrl,
+          ),
+        ),
+        Provider<SecureStorage>(
+          create: (BuildContext context) => SecureStorageImpl(),
+        ),
+        Provider<ExampleRepository>(
+          create: (BuildContext context) => ExampleRepository(
+            swapiApi: context.read(),
+          ),
+        ),
       ],
       child: child,
     );
